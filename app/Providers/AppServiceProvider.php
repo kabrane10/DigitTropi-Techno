f20178f56292dffde0545e69d841ecb3e985867f
kabranehenry <kabranehenry@gmail.com>
2026-04-17 21:58:08 +0000

    Ajout de la fonction boot dans Providers/AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,7 +19,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+{
+    if (config('env') !== 'local') {
+        URL::forceScheme('https');
     }
+}
 }
