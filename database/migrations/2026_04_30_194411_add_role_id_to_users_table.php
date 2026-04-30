c4cf14d43f16d8735643269b024c78059c7bb6d9
kabranehenry <kabranehenry@gmail.com>
2026-04-30 20:18:06 +0000

    Ajout de la colonne role_id inexistante à la table users

---
+++ database/migrations/2026_04_30_194411_add_role_id_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // On ajoute la colonne role_id après l'id avec une valeur par defaut (1) 
+            // pour tous les utilisateurs existants
+           $table->foreignId('role_id')->after('id')->default(1)->constrained('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            // 1. On supprime la contrainte de clé étrangère
+            $table->dropForeign(['role_id']);
+
+            // 2. On supprime la colonne
+            $table->dropColumn('role_id');
+        });
+    }
+};
