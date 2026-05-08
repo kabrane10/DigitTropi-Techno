261f3f63bd68fbcb31b7b3f566def1ebbe959061
kabranehenry <kabranehenry@gmail.com>
2026-05-08 21:04:49 +0000

    Fonctionnalité de la génération automatique des ref factures achats

--- resources/views/admin/distributions/print.blade.php
+++ resources/views/admin/distributions/print.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Distribution de semences - {{ $distribution->code_distribution }}</title>
+    <link rel="icon" href="{{ asset('images/favicon.png') }}">
     <style>
         * {
             margin: 0;
